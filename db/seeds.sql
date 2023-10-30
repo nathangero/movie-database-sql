0dba179d35273ca2ce37a9323b91c826b5dd4dbc
Anthony Nguyen <anthonynguye56@gmail.com>
2023-10-30 13:18:42 -0700

    created tables and values

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO movies(movie_name)
+VALUES ("Loki"),
+        ("Jurassic Park"),
+        ("Iron Man"),
+    
+INSERT INTO reviews(review, movie_id)
+VALUES ("AMAAZING TOM HIDDLESTON", 1),
+        ("ROAR", 2),
+        ("I am Iron MAN!", 3)
